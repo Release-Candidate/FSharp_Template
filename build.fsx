d44cf0ab0c8226267b65e13e6664f603d007b3af
Release-Candidate <rec@gmx.at>
2021-03-30 23:53:37 +0200

    Added Fake targets

---
+++ build.fsx
@@ -0,0 +1,152 @@
+// SPDX-License-Identifier: MIT
+// Copyright (C) 2021 Roland Csaszar
+// File: build.fsx
+//
+//==============================================================================
+
+#r "paket:
+nuget AltCover.Api >= 7.0
+nuget Fake.DotNet.Cli
+nuget Fake.IO.FileSystem
+nuget Fake.Core.Target //"
+#load ".fake/build.fsx/intellisense.fsx"
+
+open Fake.Core
+open Fake.DotNet
+open Fake.IO
+open Fake.IO.Globbing.Operators
+open Fake.Core.TargetOperators
+
+
+Target.initEnvironment ()
+
+//==============================================================================
+//                               Clean
+//
+Target.create "Clean" (fun _ ->
+    !! "src/**/bin"
+    ++ "src/**/obj"
+    |> Shell.cleanDirs
+)
+
+
+//==============================================================================
+//                               Lint
+//
+let execFSharpLint projFile = DotNet.exec id "fsharplint" (
+                                        sprintf "lint %s" projFile)
+                                        |> ignore
+
+Target.create "Lint" (fun _ ->
+    !! "src/**/*.*proj"
+    ++ "tests/**/*.*proj"
+    |> Seq.iter execFSharpLint
+)
+
+
+//==============================================================================
+//                               Build
+//
+Target.create "Build" (fun _ ->
+    !! "src/**/*.*proj"
+    ++ "tests/**/*.*proj"
+    |> Seq.iter (DotNet.build id)
+)
+
+//==============================================================================
+//                               Tests
+//
+let execTests projFile = DotNet.exec id "run" (
+                                        sprintf "--project %s" projFile)
+                                        |> ignore
+
+Target.create "Tests" (fun _ ->
+    !! "tests/**/*.*proj"
+    |> Seq.iter execTests
+)
+
+//==============================================================================
+//                           Tests & Coverage
+//
+let execTestsCoverage projFile = DotNet.exec id "test" (
+                                    sprintf "/p:AltCover=true %s" projFile)
+                                    |> ignore
+
+Target.create "TestsCoverage" (fun _ ->
+    !! "tests/**/*.*proj"
+    |> Seq.iter execTestsCoverage
+)
+
+
+//==============================================================================
+//                             Packages
+//
+let execMakePackage projFile = DotNet.exec id "pack" projFile |> ignore
+
+Target.create "Packages" (fun _ ->
+    !! "src/**/*.*proj"
+    ++ "tests/**/*.*proj"
+    |> Seq.iter execMakePackage
+)
+
+//==============================================================================
+//                Tests & Coverage Altcover Coverage API
+//
+//open AltCover.Fake.DotNet // extension method WithAltCoverOptions
+//let ForceTrue = AltCover.DotNet.CLIOptions.Force true
+
+//let p =
+//  { AltCover.Primitive.PrepareOptions.Create() with
+//      CallContext = [| "[Fact]"; "0" |]
+//      AssemblyFilter = [| "xunit" |]
+//      Report = "" }
+
+//let prepare = AltCover.AltCover.PrepareOptions.Primitive p
+//let c = AltCover.Primitive.CollectOptions.Create()
+//let collect = AltCover.AltCover.CollectOptions.Primitive c
+
+//Target.create "TestsCoverage" (fun _ ->
+//    !! "tests/**/*.*proj"
+//    |> Seq.iter (DotNet.test
+//            (fun to' -> to'.WithAltCoverOptions prepare collect ForceTrue)
+//    )
+//)
+
+//==============================================================================
+//                      Package Fake 4 API
+//
+//open Fake.DotNet.NuGet
+//Target.create "Package" (fun _ ->
+//    copyFiles packagingOutputDirectory allFilesToPackage
+//    NuGet.NuGet (fun p ->
+//        { p with
+//            Version = buildVersion
+//            Authors = authors
+//            Project = projectName
+//            Summary = projectSummary
+//            Description = projectDescription
+//            WorkingDir = packagingDirectory
+//            OutputPath = artifactOutputDirectory
+//            AccessKey = myAccessKey
+//            Publish = true })
+//        "template.nuspec"
+//)
+
+//==============================================================================
+//                              All
+//
+Target.create "All" ignore
+
+
+//==============================================================================
+//                              Targets
+//
+"Clean"
+  ==> "Build"
+  ==> "Lint"
+  ==> "Tests"
+  ==> "TestsCoverage"
+  ==> "Packages"
+  ==> "All"
+
+Target.runOrDefault "All"
